4518c4139aa763ddc265b1d18aede02308b70e61
Isaiah Santala <Isantala@yahoo.com>
2019-11-22 11:56:08 -0800

    basic player movement is functioning

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,60 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Board from './Board'
 
 const App = () => {
+  const [ gameInitialized, setGameInitialized ] = useState(false)
+  const [ playerLoc, setPlayerLoc ] = useState({ x: 5, y: 5 })
 
-  const [ board, setBoard ] = useState([
+  const updatePlayerLoc = ({ key }) => {
+    setPlayerLoc(previousLoc => {
+      let { x, y } = previousLoc
+      if (key === 'a') y = y - 1
+      if (key === 'd') y = y + 1
+      if (key === 'w') x = x - 1
+      if (key === 's') x = x + 1
+      return { x, y }
+    })
+  }
+
+  const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ])
+
+  const updateBoard = ({ x, y }) => {
+    const newBoard = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    newBoard[y][x] = 1
+    setBoard(newBoard)
+  }
+
+  useEffect(() => {
+    if (!gameInitialized) {
+      document.addEventListener('keydown', updatePlayerLoc)
+      setGameInitialized(true)
+    }
+    updateBoard(playerLoc)
+  }, [playerLoc])
 
   return (
     <div>
