54d20ab36b90fcc2a95e37b82c9e042fd2c1b0c7
Isaiah Santala <Isantala@yahoo.com>
2019-11-22 14:59:53 -0800

    finished all core functionality

--- src/components/App.js
+++ src/components/App.js
@@ -3,38 +3,74 @@ import BoardView from './BoardView'
 import { generateBoard } from '../game/generateBoard' 
 
 const App = () => {
+  const [ direction, setDirection ] = useState(null)
+  const [ gameOver, setGameOver ] = useState(false)
   const [ gameInitialized, setGameInitialized ] = useState(false)
-  const [ playerLoc, setPlayerLoc ] = useState([{ x: 5, y: 5 }])
-  const [ food, setFood ] = useState([{ x: 2, y: 2 }])
+  const [ board, setBoard ] = useState(generateBoard())
+  const [ playerLoc, setPlayerLoc ] = useState([{ x: Math.floor(board[0].length / 2), y: Math.floor(board.length / 2), isNew: true }])
+  const [ food, setFood ] = useState({ x: 2, y: 2 })
 
-  const updatePlayer = () => {
-    //see if player is touching food
-    //if so increase player length and delete that food
-
-    food.forEach((f_coordinates, i) => {
-      const { x, y } = f_coordinates
-      playerLoc.forEach(p_coordinates => {
-        if (p_coordinates.x === x && p_coordinates.y === y) { 
-
-          const newPlayerLoc = playerLoc.map(coordinates => coordinates)
-          newPlayerLoc.unshift({ x, y })
-          setPlayerLoc(newPlayerLoc)
-
-          setFood(food.filter((coordinates, idx) => i !== idx))
-        }
-      })
+  const didPlayerEat = () => {
+    const { x, y } = food
+    playerLoc.forEach(coordinates => {
+      if (coordinates.x === x && coordinates.y === y) {
+        generateNewFood() 
+        grow()
+      }
     })
   }
 
-  const updatePlayerLoc = ({ key }) => {
+  const grow = () => {
+    const { x, y } = food
+    const newPlayerLoc = [{ x, y, isNew: true }, ...playerLoc.map(coordinates => coordinates)]
+    newPlayerLoc[1].isNew = false
+    setPlayerLoc(newPlayerLoc)
+  }
+
+  const didPlayerDie = () => {
+    const [{ x, y, isNew }, ...tail ] = playerLoc
+    tail.forEach(coordinates => {
+      if (coordinates.x === x && coordinates.y === y && !isNew) setGameOver(true)
+    })
+    if (y < 0 || y > board.length - 1) return setGameOver(true)
+    if (x < 0 || x > board[0].length - 1) return setGameOver(true)
+    updateBoard()
+    didPlayerEat()
+  }
+
+  const generateNewFood = () => {
+    const possible = []
+    for (let y = 0; y < board.length; y++) {
+      for (let x = 0; x < board[y].length; x++) {
+        if (board[y][x] === 0) possible.push({ x, y })
+      }
+    }
+    const idx = Math.floor(Math.random() * (possible.length - 1))
+    console.log(idx, possible.length)
+    setFood(possible[idx])
+  }
+
+  const updateDirection =({ key }) => {
+    let newDirection = ''
+    if (key === 'a') newDirection = 'LEFT'
+    if (key === 'd') newDirection = 'RIGHT'
+    if (key === 'w') newDirection = 'UP'
+    if (key === 's') newDirection = 'DOWN'
+    setDirection(previous => {
+      if (previous !== newDirection) updatePlayerLoc(newDirection)
+      return newDirection
+    })
+  }
+
+  const updatePlayerLoc = (direction) => {
     setPlayerLoc(previousLoc => {
       return previousLoc.map((coordinates, i) => {
         if (i === previousLoc.length - 1) {
           let { x, y } = coordinates
-          if (key === 'a') y = y - 1
-          if (key === 'd') y = y + 1
-          if (key === 'w') x = x - 1
-          if (key === 's') x = x + 1
+          if (direction === 'LEFT') y = y - 1
+          if (direction === 'RIGHT') y = y + 1
+          if (direction === 'UP') x = x - 1
+          if (direction === 'DOWN') x = x + 1
           return { x, y }
         }
         return previousLoc[i + 1]
@@ -42,14 +78,9 @@ const App = () => {
     })
   }
 
-  const [board, setBoard] = useState(generateBoard())
-
   const updateBoard = () => {
     const newBoard = generateBoard()
-    food.forEach(coordinates => {
-      const { x, y } = coordinates
-      newBoard[y][x] = 2
-    })
+    newBoard[food.y][food.x] = 2
     playerLoc.forEach(coordinates => {
       const { x, y } = coordinates
       newBoard[y][x] = 1
@@ -57,18 +88,29 @@ const App = () => {
     setBoard(newBoard)
   }
 
+  const step = (newDirection) => {
+    updatePlayerLoc(newDirection)
+    setTimeout(() => {
+      setDirection(direction => {
+        step(direction)
+        return direction
+      })
+    }, 250)
+  }
+
   useEffect(() => {
     if (!gameInitialized) {
-      document.addEventListener('keydown', updatePlayerLoc)
+      step(direction)
+      document.addEventListener('keydown', updateDirection)
       setGameInitialized(true)
     }
-    updateBoard()
-    updatePlayer()
-  }, [playerLoc])
+    didPlayerDie()
+  }, [playerLoc, setFood])
 
   return (
     <div>
-      <BoardView board={board}/>
+      {gameOver ? <div>game over</div> 
+      : <BoardView board={board}/>}
     </div>
   )
 }
